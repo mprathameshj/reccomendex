66f69e992b9ec86fb5a8dde036e314656711a2be
mprathameshj <prathameshjaybhaye00@gmail.com>
2024-07-17 21:07:57 +0530

    Initial commit to github

---
+++ src/main/java/com/example/Reccomendex/ReccomendexApplication.java
@@ -0,0 +1,12 @@
+package com.example.Reccomendex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReccomendexApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReccomendexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Reccomendex/ReccomendexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reccomendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReccomendexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
