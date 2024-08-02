22a97ebbaaeb7c7a2cdbc512fe39f63fd5455b74
mprathameshj <prathameshjaybhaye00@gmail.com>
2024-08-02 15:21:48 +0530

    Added the constants package and FunctionConstant class with values

---
+++ src/main/java/com/example/Reccomendex/Constants/FunctionConstants.java
@@ -0,0 +1,18 @@
+package com.example.Reccomendex.Constants;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FunctionConstants {
+    public static final int RAM_MIN=1;
+    public static final int RAM_MAX=20;
+    public static final int CAMERA_MIN=1;
+    public static final int CAMERA_MAX=120;
+    public static final int BATTERY_MIN=2500;
+    public static final int BATTERY_MAX=10000;
+    public static final int PRICE_MIN=8000;
+    public static final int PRICE_MAX=200000;
+    public static final int PROCESSOR_MIN=1;
+    public static final int PROCESSOR_MAX=8;
+    public static final int SCREEN_MIN=5;
+    public static final int SCREEN_MAX=10;
+}
